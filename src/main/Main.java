0fd1222ccd723a40e83fc92caf76a3e381137a6a
Grzegorz Hauska <grzegorz.hauska@gmail.com>
2013-11-13 22:33:16 +0100

    xxx

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,9 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.TreeSet;
 
 public class Main {
 
@@ -15,20 +18,28 @@ public class Main {
 		int country;
 		float starrating;
 		float usd;
-		public CustomRow(int searchId, int propId, boolean bookingBool,
-				int country, float starrating, float usd) {
+
+		 
+		public CustomRow(ResultSet rs) {
 			super();
-			this.searchId = searchId;
-			this.propId = propId;
-			this.bookingBool = bookingBool;
-			this.country = country;
-			this.starrating = starrating;
-			this.usd = usd;
+			try {
+				this.searchId = rs.getInt("srch_id");
+			
+			this.propId = rs.getInt("prop_id");
+			this.bookingBool = rs.getBoolean("booking_bool");
+			this.country = rs.getInt("visitor_location_country_id");
+			this.starrating = rs.getFloat("visitor_hist_starrating");
+			this.usd = rs.getFloat("visitor_hist_adr_usd");
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-		
+
 	}
-	
+
 	private Connection conn;
+	private ArrayList<CustomRow> all = new ArrayList<CustomRow>();
 
 	/**
 	 * @param args
@@ -42,45 +53,87 @@ public class Main {
 	private float distance(CustomRow cs, CustomRow cs1) {
 		// TODO Auto-generated method stub
 		float distance = 0;
+		if (cs.country != cs1.country)
+			distance++;
 
-		try {
-			
+		distance += Math.abs(cs.usd - cs1.usd) / 100;
+		distance += Math.abs(cs.starrating - cs1.starrating);
 
-			if (cs.country != cs1.country) distance++;
-			
-			distance+= Math.abs(usd-usd1)/100;
-			distance+= Math.abs(starrating-starrating1);
-			
-			if(usd==0 || usd1==0) distance+=5;
-			if(starrating==0 || starrating1==0) distance+=5;
-			
-			return distance;
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return 10000;
-		
+		if (cs.usd == 0 || cs1.usd == 0)
+			distance += 5;
+		if (cs.starrating == 0 || cs1.starrating == 0)
+			distance += 5;
+
+		return distance;
 
 	}
+	
+	class Tuple<CustomRow, Float> { 
+	    public final CustomRow x; 
+	    public final Float y; 
+	    public Tuple(CustomRow x, Float y) { 
+	      this.x = x; 
+	      this.y = y; 
+	    } 
+	  }
 
-	private void findNear(ResultSet rs) {
+	
+
+	private void findNear(CustomRow cs) {
 		// TODO Auto-generated method stub
-		List
+		TreeSet<Tuple<CustomRow, Float>> knn = new TreeSet<Tuple<CustomRow, Float>>(new Comparator<Tuple<CustomRow, Float>>() {
+			@Override
+			public int compare(Tuple<CustomRow, java.lang.Float> o1,
+					Tuple<CustomRow, java.lang.Float> o2) {
+				// TODO Auto-generated method stub
+				return o1.y > o2.y ? 1 : o1.y == o2.y ? 0 : -1;
+			}
+		});
+		
+		for(CustomRow cs2 : all){
+			if(knn.size() >= 50){
+				CustomRow cs
+			}
+		}
+		
+		
 	}
 
 	private void start() {
 		// TODO Auto-generated method stub
-		String query = "select * from testset order by rand() limit 10000";
-		String selectAll = "select  from testset";
+
+		String query = "select srch_id, prop_id, visitor_location_country_id, visitor_hist_starrating, visitor_hist_adr_usd, booking_bool from train_set order by rand() limit 10000";
+		String selectAll = "select srch_id, prop_id, visitor_location_country_id, visitor_hist_starrating, visitor_hist_adr_usd, booking_bool from train_set";
+
 		ResultSet rs;
+
+		System.out.println("Start loading data...");
+		  try {
+		   PreparedStatement p = conn.prepareStatement(selectAll);
+		   rs = p.executeQuery();
+		   int  i=0;
+		   while (rs.next()) {
+		    all.add(new CustomRow(rs));
+		    i++;
+		    if(i % 10000 == 0) System.out.println(i);
+		    
+		   }
+		  } catch (SQLException e) {
+		   // TODO Auto-generated catch block
+		   e.printStackTrace();
+		  }
+		  System.out.println("Data loading finished");
+		/*
+		 * String query = "select * from testset order by rand() limit 10000";
+		 * String selectAll = "select  from testset";
+		 */
 
 		try {
 			PreparedStatement p = conn.prepareStatement(query);
 			rs = p.executeQuery();
 
 			while (rs.next()) {
-				findNear(rs);
+				findNear(new CustomRow(rs));
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
